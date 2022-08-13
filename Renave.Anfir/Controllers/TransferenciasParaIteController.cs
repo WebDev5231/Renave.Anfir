cbe4bcf3a3ddb1a118cfdcb0e01abb1d242768e2
Renato <renatotrincas@gmail.com>
2022-08-13 14:38:51 -0300

    Transferências Para ITE

---
+++ Renave.Anfir/Controllers/TransferenciasParaIteController.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Renave.Anfir.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Renave.Anfir.Controllers
+{
+    public class TransferenciasParaIteController : ApiController
+    {
+        private string basePath = ConfigurationManager.AppSettings["SerproRenaveApiUrl"];
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post([FromBody] Solicitacao solicitacao)
+        {
+            try
+            {
+                var url = basePath + "/api/ite/transferencias-para-ite";
+
+                using (var client = new HttpClient())
+                {
+                    var json = JsonConvert.SerializeObject(solicitacao);
+
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(url),
+                        Headers = {
+                        { "Accept", "application/json" },
+                    },
+                        Content = new StringContent(json)
+                        {
+                            Headers = {
+                            ContentType = new MediaTypeHeaderValue("application/json")
+                            }
+                        }
+                    };
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var jsonString = response.Content.ReadAsStringAsync();
+                            var retorno = JsonConvert.DeserializeObject<object>(jsonString.Result);
+
+                            return Request.CreateResponse(retorno);
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(HttpStatusCode.BadRequest);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ Renave.Anfir/Models/Solicitacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Renave.Anfir.Models
+{
+    public class Solicitacao
+    {
+        public string chaveNotaFiscalRemessa { get; set; }
+        public string codigoClienteMontadora { get; set; }
+        public string cpfOperadorResponsavel { get; set; }
+        public string dataHoraMedicaoHodometro { get; set; }
+        public string dataTransferenciaEstoque { get; set; }
+        public int idAutorizacao { get; set; }
+        public int quilometragemHodometro { get; set; }
+        public double valorProduto { get; set; }
+    }
+}
