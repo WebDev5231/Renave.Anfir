216ac489605c043e3c4cd85b62b7d64033b8c049
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2022-08-15 17:09:13 -0300

    Update EmpresaRenaveCertificado.cs

--- Renave.Anfir.Model/EmpresaRenaveCertificado.cs
+++ Renave.Anfir.Model/EmpresaRenaveCertificado.cs
@@ -9,6 +9,7 @@ namespace Renave.Anfir.Model
     public class EmpresaRenaveCertificado
     {
         public int ID_Empresa { get; set; }
+        public string Data_inclusao { get; set; }
         public string CertificadoFileName { get; set; }
         public string CertificadoPassword { get; set; }
     }
