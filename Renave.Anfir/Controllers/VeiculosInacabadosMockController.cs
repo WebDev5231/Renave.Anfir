89708f1259bf7544fe7afb9e0ed6084f65688c9e
Renato <renatotrincas@gmail.com>
2022-08-19 10:46:09 -0300

    Mocks de veículo inacabado

---
+++ Renave.Anfir/Controllers/VeiculosInacabadosMockController.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Renave.Anfir.Business;
+using Renave.Anfir.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Renave.Anfir.Controllers
+{
+    /// <summary>
+    /// Complementar(tipo montagem = acabado) mock de veículo em homologação.
+    /// </summary>
+    public class VeiculosInacabadosMockController : ApiController
+    {
+        private string basePath = ConfigurationManager.AppSettings["SerproRenaveApiUrl"];
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post([FromBody]int ID_Empresa, string chassi)
+        {
+            try
+            {
+                var url = basePath + "/api/ite/veiculos-inacabados-mock/acabar";
+
+                var certificadoBusiness = new CertificadoBusiness();
+                var handler = certificadoBusiness.GetHandler(ID_Empresa);
+
+                using (var client = new HttpClient(handler))
+                {
+                    var json = JsonConvert.SerializeObject(chassi);
+
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        RequestUri = new Uri(url),
+                        Headers = {
+                        { "Accept", "application/json" },
+                    },
+                        Content = new StringContent(json)
+                        {
+                            Headers = {
+                            ContentType = new MediaTypeHeaderValue("application/json")
+                            }
+                        }
+                    };
+
+                    using (var response = await client.SendAsync(request))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return Request.CreateResponse(HttpStatusCode.OK);
+                        }
+                        else if (response.StatusCode == (HttpStatusCode)422)
+                        {
+                            var jsonString = response.Content.ReadAsStringAsync();
+                            var retorno = JsonConvert.DeserializeObject<ErroRetorno>(jsonString.Result);
+
+                            return Request.CreateResponse((HttpStatusCode)422, retorno);
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(response.StatusCode, response.Content.ReadAsStringAsync());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
