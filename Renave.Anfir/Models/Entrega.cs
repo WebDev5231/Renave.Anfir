6e6ad072b8fb729c282fa62aed92e3a3d5a11ff2
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2023-03-17 10:45:39 -0300

    Update Entrega.cs

--- Renave.Anfir/Models/Entrega.cs
+++ Renave.Anfir/Models/Entrega.cs
@@ -12,9 +12,9 @@ namespace Renave.Anfir.Models
         public string cnpjEstabelecimentoEntregador { get;}
         public string cnpjEstabelecimentoVendedor { get; set; }
         public string estado { get; set; }
-        public long id { get; set; }
-        public long idEntregaGeradaNoCancelamentoEntrega { get; set; }
-        public long idEntregaOrigemCancelamentoEntrega { get; set; }
+        public long? id { get; set; }
+        public long? idEntregaGeradaNoCancelamentoEntrega { get; set; }
+        public long? idEntregaOrigemCancelamentoEntrega { get; set; }
         public RealizacaoEntrega realizacaoEntrega { get; set; }
     }
 }
