c2be0c145fbe233a9543253955197821c47e4edd
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2024-05-29 14:52:25 -0300

    att certificados
    att certificados

---
+++ Renave.Anfir/Controllers/VeiculosTransformadosITEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Renave.Anfir.Controllers
+{
+    public class VeiculosTransformadosITEController : Controller
+    {
+        // GET: VeiculosTransformadosITE
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
